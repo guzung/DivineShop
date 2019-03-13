afa04977bee0c46fa703c904614918ec0e9b8ac2
Gheorghe Guzun <gheorghe.guzun@bass.md>
2019-03-13 10:06:16 +0200

    project skeleton

---
+++ backend/src/main/java/com/guzung/divineshop/DivineShopApplication.java
@@ -0,0 +1,13 @@
+package com.guzung.divineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivineShopApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/guzung/divineshop/DivineShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guzung.divineshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DivineShopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
